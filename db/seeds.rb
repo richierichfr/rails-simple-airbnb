994a6b3937b7406f9ef73804432e065659591a36
ulrich aguey <ulrichatlanta@hotmail.com>
2024-01-20 10:49:18 +0100

    exerice2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Gorgeous house by the Rocky Mountain',
+  address: '10 Clifton denver Road',
+  description: 'Visit the rocky Mountain ',
+  price_per_night: 150,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Luxury apartement skycrapers NYC',
+  address: '1 avenue brooklyn park',
+  description: 'a luxury flat that has everything, even a great view on the Madisaon Square Garden',
+  price_per_night: 1000,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Romantic flat in Paris',
+  address: '10 avenues des champs élysées',
+  description: 'spend all your time in Paris, close to the world known champs élysées',
+  price_per_night: 35,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
